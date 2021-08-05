f055bf9f5c6b30a8017ac7389f774a0041637f25
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-05 11:20:33 -0300

    Topic details endpoint

--- src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -8,12 +8,14 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.TopicDetailsDto;
 import br.com.alura.forum.controller.dto.TopicDto;
 import br.com.alura.forum.controller.form.TopicForm;
 import br.com.alura.forum.model.Topic;
@@ -48,6 +50,12 @@ public class TopicsController {
 		
 		URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicDto(topic));
+	}
+	
+	@GetMapping("/{id}")
+	public TopicDetailsDto detail(@PathVariable Long id) {
+		Topic topic = topicRepository.getById(id);
+		return new TopicDetailsDto(topic);
 	}
 	
 }
