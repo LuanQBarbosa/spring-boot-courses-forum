d6defe6fb6e01f8f711e43528bc0478c5b9f5b41
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-05 12:15:22 -0300

    Id existance validation

--- src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -2,12 +2,14 @@ package br.com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +49,7 @@ public class TopicsController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicDto> register(@RequestBody @Valid TopicForm form, UriComponentsBuilder uriBuilder) {
 		Topic topic = form.convert(courseRepository);
 		topicRepository.save(topic);
@@ -56,17 +59,37 @@ public class TopicsController {
 	}
 	
 	@GetMapping("/{id}")
-	public TopicDetailsDto detail(@PathVariable Long id) {
-		Topic topic = topicRepository.getById(id);
-		return new TopicDetailsDto(topic);
+	public ResponseEntity<TopicDetailsDto> detail(@PathVariable Long id) {
+		Optional<Topic> topic = topicRepository.findById(id);
+		if (topic.isPresent()) {
+			return ResponseEntity.ok(new TopicDetailsDto(topic.get()));
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 	@PutMapping("/{id}")
 	@Transactional
 	public ResponseEntity<TopicDto> update(@PathVariable Long id, @RequestBody @Valid TopicUpdateForm form) {
-		Topic topic = form.update(id, topicRepository);
+		Optional<Topic> optional = topicRepository.findById(id);
+		if (optional.isPresent()) {
+			Topic topic = form.update(id, topicRepository);
+			return ResponseEntity.ok(new TopicDto(topic));
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Optional<Topic> optional = topicRepository.findById(id);
+		if (optional.isPresent()) {
+			topicRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
 		
-		return ResponseEntity.ok(new TopicDto(topic));
+		return ResponseEntity.notFound().build();
 	}
 	
 }
