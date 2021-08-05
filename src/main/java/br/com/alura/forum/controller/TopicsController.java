0010bf6877564c34042b0ec48f784e19a3622a08
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-05 11:51:28 -0300

    Topic update endpoint

--- src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import br.com.alura.forum.controller.dto.TopicDetailsDto;
 import br.com.alura.forum.controller.dto.TopicDto;
 import br.com.alura.forum.controller.form.TopicForm;
+import br.com.alura.forum.controller.form.TopicUpdateForm;
 import br.com.alura.forum.model.Topic;
 import br.com.alura.forum.repository.CourseRepository;
 import br.com.alura.forum.repository.TopicRepository;
@@ -56,6 +59,14 @@ public class TopicsController {
 	public TopicDetailsDto detail(@PathVariable Long id) {
 		Topic topic = topicRepository.getById(id);
 		return new TopicDetailsDto(topic);
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicDto> update(@PathVariable Long id, @RequestBody @Valid TopicUpdateForm form) {
+		Topic topic = form.update(id, topicRepository);
+		
+		return ResponseEntity.ok(new TopicDto(topic));
 	}
 	
 }
