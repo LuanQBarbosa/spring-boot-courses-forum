95dbed34239fa31eef4eaa188d91a962e0ebafe2
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-06 08:37:22 -0300

    Generating token when user authenticates

--- src/main/java/br/com/alura/forum/controller/form/LoginForm.java
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 	
 	private String email;
@@ -19,6 +21,10 @@ public class LoginForm {
 	
 	public String getPassword() {
 		return password;
+	}
+
+	public UsernamePasswordAuthenticationToken convert() {
+		return new UsernamePasswordAuthenticationToken(email, password);
 	}
 
 }
