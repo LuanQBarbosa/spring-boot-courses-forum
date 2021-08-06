a2732e1239609448d755286974d464f6840fd6fd
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-06 08:14:50 -0300

    Initial setup to use JWT instead of sessions for authentication

---
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.controller.form;
+
+public class LoginForm {
+	
+	private String email;
+	private String password;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+}
--- src/main/java/br/com/alura/forum/model/Profile.java
+++ src/main/java/br/com/alura/forum/model/Profile.java
@@ -14,7 +14,6 @@ public class Profile implements GrantedAuthority {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String name;
 
 	public Long getId() {
